1b4b23c0138c490fe5e0f4901e8a33a635bfb4ec
rookantha <rookantha.vc@gmail.com>
2025-02-19 20:20:54 +0530

    port change

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors();
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
-  console.log(`🚀 Server is running on port ${port}`);
+  await app.listen(3002);
 }
 bootstrap();
